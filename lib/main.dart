c98308a63598dc763ec1b105f66e180e881e5c8f
Yusuke <yhakamaya@adobe.com>
2023-05-20 21:19:49 +0900

    convert Home to StatefulWidget

--- lib/main.dart
+++ lib/main.dart
@@ -18,11 +18,16 @@ class MainApp extends StatelessWidget {
   }
 }
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({
     super.key,
   });
 
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
