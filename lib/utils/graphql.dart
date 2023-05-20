e5d222c38f26071f2f493f8bdfae10b7287ba326
Yusuke <yhakamaya@adobe.com>
2023-05-20 22:14:18 +0900

    add GraphQL utils

---
+++ lib/utils/graphql.dart
@@ -0,0 +1,5 @@
+class GraphQL {
+  static const endpointAuthority = 'publish-p111360-e1081459.adobeaemcloud.com';
+  static const endpointPath = 'graphql/execute.json/wknd-shared/';
+  static const queryBeveragesAll = 'beverages-all';
+}
