a53f62f9f39ecbe217bc4b3deebc8ee3c9d55f59
Yusuke <yhakamaya@adobe.com>
2023-05-20 23:18:56 +0900

    add queryFoodsAll

--- lib/utils/graphql.dart
+++ lib/utils/graphql.dart
@@ -2,4 +2,5 @@ class GraphQL {
   static const endpointAuthority = 'publish-p111360-e1081459.adobeaemcloud.com';
   static const endpointPath = 'graphql/execute.json/wknd-shared/';
   static const queryBeveragesAll = 'beverages-all';
+  static const queryFoodsAll = 'foods-all';
 }
