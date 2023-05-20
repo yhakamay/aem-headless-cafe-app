ad5bb8a7a8136163ac0dcd01a89d01602d1b8baf
Yusuke <yhakamaya@adobe.com>
2023-05-20 22:27:53 +0900

    add error handler

--- lib/components/pages/home.dart
+++ lib/components/pages/home.dart
@@ -19,19 +19,24 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   List<Beverage> _beverages = [];
   bool _isLoading = true;
+  String? _error;
 
   Future<void> fetchItems() async {
     // https://publish-p111360-e1081459.adobeaemcloud.com/graphql/execute.json/wknd-shared/beverages-all
-    final response = await http.get(Uri.https(
-      GraphQL.endpointAuthority,
-      '${GraphQL.endpointPath}${GraphQL.queryBeveragesAll}',
-    ));
-    final json = jsonDecode(response.body);
-    final items = json['data']['beverageList']['items'];
+    try {
+      final response = await http.get(Uri.https(
+        GraphQL.endpointAuthority,
+        '${GraphQL.endpointPath}${GraphQL.queryBeveragesAll}',
+      ));
+      final json = jsonDecode(response.body);
+      final items = json['data']['beverageList']['items'];
 
-    _beverages = items.map<Beverage>((item) {
-      return Beverage.fromJson(item);
-    }).toList();
+      _beverages = items.map<Beverage>((item) {
+        return Beverage.fromJson(item);
+      }).toList();
+    } on Exception catch (e) {
+      _error = e.toString();
+    }
   }
 
   @override
@@ -50,37 +55,39 @@ class _HomeState extends State<Home> {
       appBar: AppBar(
         title: const Text('AEM Headless Cafe'),
       ),
-      body: _isLoading
-          ? const Center(
-              child: SizedBox(
-                width: 160,
-                child: LinearProgressIndicator(),
-              ),
-            )
-          : ListView.builder(
-              itemCount: _beverages.length,
-              itemBuilder: (context, index) {
-                final beverage = _beverages[index];
-                return ListTile(
-                  title: Text(beverage.title),
-                  subtitle: Text(
-                    beverage.description?.plaintext ?? '',
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
+      body: _error != null
+          ? Center(child: Text(_error!))
+          : _isLoading
+              ? const Center(
+                  child: SizedBox(
+                    width: 160,
+                    child: LinearProgressIndicator(),
                   ),
-                  trailing: ClipRRect(
-                    borderRadius: BorderRadius.circular(8),
-                    child: FadeInImage.memoryNetwork(
-                      image:
-                          'https://${GraphQL.endpointAuthority}${beverage.primaryImage.path}',
-                      placeholder: kTransparentImage,
-                      width: 50,
-                      height: 50,
-                    ),
-                  ),
-                );
-              },
-            ),
+                )
+              : ListView.builder(
+                  itemCount: _beverages.length,
+                  itemBuilder: (context, index) {
+                    final beverage = _beverages[index];
+                    return ListTile(
+                      title: Text(beverage.title),
+                      subtitle: Text(
+                        beverage.description?.plaintext ?? '',
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      trailing: ClipRRect(
+                        borderRadius: BorderRadius.circular(8),
+                        child: FadeInImage.memoryNetwork(
+                          image:
+                              'https://${GraphQL.endpointAuthority}${beverage.primaryImage.path}',
+                          placeholder: kTransparentImage,
+                          width: 50,
+                          height: 50,
+                        ),
+                      ),
+                    );
+                  },
+                ),
     );
   }
 }
